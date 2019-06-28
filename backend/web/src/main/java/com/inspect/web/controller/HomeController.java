443329f75e1b077dc0de5ced9d377ae4c1e9bfbd
choi94 <ehdgns94@icloud.com>
2019-06-28 09:04:38 +0900

    Project Setting

---
+++ backend/web/src/main/java/com/inspect/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.inspect.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value="/")
+    public String home(){
+        return "index";
+    }
+    
+}
