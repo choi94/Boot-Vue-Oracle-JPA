cc0c298262fefcd8ba08335c80c3fd8d228b6f75
choi94 <ehdgns94@icloud.com>
2019-06-28 09:30:49 +0900

    Create Directory

--- backend/web/src/main/java/com/inspect/web/home/controller/HomeController.java
+++ backend/web/src/main/java/com/inspect/web/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.inspect.web.controller;
+package com.inspect.web.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
